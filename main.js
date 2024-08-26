51269f2472f55844f27baf27716b48f0382dbfdb
rweadpowerr <readpower63@gmail.com>
2024-08-26 13:47:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 function heapSpray(size, count) {
-  let sprays = []; 
-  let bufferArray = []; 
-  let counter = 0;
+var sprays = []; 
+    for (var i = 0; i < count; i++) {
+    let largeString = "A".repeat(1000000); // 1 million A's
+    let obj = {};     largeString = "A".repeat(1000000);
+        // Array buffer allocation
+        bufferArray.push(new ArrayBuffer(1000000));
+        if (bufferArray.length > 1000) {
+            bufferArray = [];
+        }
+        // Object property expansion
+        obj[counter] = "A".repeat(1000)
 
-  for (var i = 0; i < count; i++) 
-    // Allocate large string{
-ffer allocation
-    bufferArray.push(new ArrayBuffer(size));
-    if (bufferArray.length > 1000) {
-      bufferArray = [];
+        // Array buffer allocation
+        sprays.push(new ArrayBuffer(size));
+        // Object property expansion
     }
-
-    // Object property expansion
-    let obj = {};
-    obj[counter] = "A".repeat(1000);
-    counter++;
-
-    // Push to sprays
-w heap spray
- as necessary
-chunkSize, sprayCount);
-or (var i = 0; i < sprays.length; i++) 
-    var ptr = sprays[i];{
-ry chunk
+}
+function run() {
+var chunksize = 0x1000; 
+var spraycount = 0x200;
+var sprays = heapSpray(chunkSize, spraycount);
+for (var i = 0; i < sprays.length; i++){ 
+    var ptr = sprays[i];
+   p.write8(ptr.add32()0x100), i);
+  return sprays;
+}
+}
     // (Note: Actual memory manipulation would depend on the environment)
     // p.write8(ptr.add32(0x100), i); // Example offset and data
